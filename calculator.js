e7c5ec9fc849817e86c39e9c1aef1a9261f7d56d
rakshitnayak <rdsonnn@gmail.com>
2024-11-27 23:15:15 +0530

    refactor 1

--- calculator.js
+++ calculator.js
@@ -1,25 +1,22 @@
-const express =require("express");
-const bodyParser=require("body-parser");
-const app=express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-app.use(bodyParser.urlencoded({extened:true}));
+app.use(bodyParser.urlencoded({ extened: true }));
 
-app.get("/",function(req,res){
-  res.sendFile(__dirname+ "/bmicalculator.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/bmicalculator.html");
 });
 
-app.post("/",function(req,res){
-  var weight=parseFloat(req.body.n1);
-  var height=parseFloat(req.body.n2);
+app.post("/", function (req, res) {
+  var weight = parseFloat(req.body.n1);
+  var height = parseFloat(req.body.n2);
 
-  var result= weight/(height*height);
+  var result = weight / (height * height);
 
   res.send("your bmi is  " + result);
 });
 
-
-
-
-app.listen(3000,function(){
+app.listen(3000, function () {
   console.log("server started in port 3000");
 });
